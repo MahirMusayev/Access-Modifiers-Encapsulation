4cb9459d0aed29284a05019beacc2383d48aefe6
Mahir Musayev <146106439+musayevmahir68@users.noreply.github.com>
2024-04-03 01:41:13 +0400

    task

---
+++ Access Modifiers, Encapsulation Task/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Access_Modifiers__Encapsulation_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(30, 20, 5);
+            bool dur = true;
+
+            while (dur)
+            {
+                Menu();
+                string num = Console.ReadLine();
+                switch (num)
+                {
+                    case "0":
+                        weapon.ShowInformation();
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        weapon.GetRemainBulletCount();
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        dur = false;
+                        break;
+                    case "T":
+                        Console.WriteLine("Enter new bullet capacity:");
+                        int newCapacity = int.Parse(Console.ReadLine());
+                        weapon.BulletCapacity = newCapacity;
+                        break;
+                    case "S":
+                        Console.WriteLine("Enter new bullet count:");
+                        int newnum  = int.Parse(Console.ReadLine());
+                        weapon.BulletNumber = newnum;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+
+                }
+                //Console.ReadKey(); 
+                //Console.Clear();
+
+
+            }
+            static void Menu()
+            {
+                Console.WriteLine("0 - İnformasiya almaq üçün\r\n1 - Shoot metodu üçün\r\n2 - Fire metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün");
+            }
+
+        }
+    }
+}
---
+++ Access Modifiers, Encapsulation Task/Weapon.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers__Encapsulation_Task
+{
+    internal class Weapon
+    {
+        public int _bulletCapacity ; 
+        public int _bulletNumber;
+        private int _bulletTime;
+        private bool _fireMood =true;
+        public int BulletCapacity
+        {
+            get { return _bulletCapacity; }
+            set { _bulletCapacity = value; }
+        }
+        public int BulletNumber
+        {
+            get { return _bulletNumber; }
+            set { _bulletNumber = value; }
+        }
+        public int BulletTime
+        {
+            get { return _bulletTime; }
+            set { _bulletTime = value; }
+        }
+        public Weapon( int BulletCapacity, int BulletNumber, int BulletTime )
+        {
+            _bulletCapacity = BulletCapacity;
+            _bulletNumber = BulletNumber;
+            _bulletTime = BulletTime;
+        }
+        public void Shoot()
+        {
+            if (BulletNumber>0)
+            {
+                Console.WriteLine("1 gulle atildi");
+                BulletNumber--;
+                Console.WriteLine(BulletNumber);
+            }
+           else if (BulletNumber == 0)
+            {
+                Console.WriteLine("gulle qutardi");
+
+            }
+        }
+        public void Fire()
+        {
+            Console.WriteLine($"{BulletNumber} gulle atildi ve gulle qutardi");
+            BulletNumber = 0;
+            
+        }
+        public void GetRemainBulletCount()
+        {
+            int full = BulletCapacity - BulletNumber;
+            Console.WriteLine("daraxin tam dolmasu ucun " + full + " gulle lazimdi");
+        }
+        public void Reload()
+        {
+            int full = BulletCapacity - BulletNumber;
+            BulletNumber = BulletNumber + full;
+            Console.WriteLine("darax yeniden dolduruldu");
+        }
+        public void ChangeFireMode()
+        {
+            if (_fireMood)
+            {
+                _fireMood = false;
+                Console.WriteLine("Fire mode changed to Automatic.");
+            }
+            else
+            {
+                _fireMood = true;
+                Console.WriteLine("Fire mode changed to Single.");
+            }
+        }
+        public void ShowInformation()
+    {
+        Console.WriteLine($"Bullet Capacity: {_bulletCapacity}");
+        Console.WriteLine($"Bullet Count: {_bulletNumber}");
+        Console.WriteLine($"Fire Mode: {_fireMood}");
+    }
+    }
+
+}
